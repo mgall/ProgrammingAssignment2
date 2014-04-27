02aa583f3905b1b9efe1909a87fe51e3de3393c2
Marco Galletti <hello@marcogalletti.com>
2014-04-27 22:39:39 +0200

    Update cachematrix.R
    Limiting column size of code

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,39 @@
-## This function creates a wrapper object for a matrix" x  that can cache its own inverse
-#      Param [x]: A Matrix that you want to be able to cache its inverse
-#      Return : An object that can be used with cacheSolve function to calculate
-#               and cache the inverse of matrix x
+## This function creates a wrapper object for a matrix" x  that can 
+# cache its own inverse,
+#      Param [x]: A Matrix which you want to compute & cache its inverse
+#      Return : An object that can be used with cacheSolve function to 
+#               calculate and cache the inverse of matrix x
 #
 # EXAMPLE:
 # invertible_m = matrix( c(1,2,3,1,4,9,1,8,27))  
 # wrapped_m = makeCacheMatrix( invertible_m )
 #
-# now wrapped_m is an object that can handle its own inverse with these properies:
-#
-# wrapped_m$get() == invertible_m
-# cacheSolve( wrapped_m ) == solve( invertible_m ) 
+# wrapped_m is an object that has this two properties:
+# 1) wrapped_m$get() == invertible_m
+# 2) cacheSolve( wrapped_m ) == solve( invertible_m ) 
 ##
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     
     # Local functions used to handle the inverse of x thorough cache
-    set <- function(val) { x <<- val; inv <<- NULL }
+    set <- function(val) { 
+        x <<- val 
+        inv <<- NULL 
+    }
     get <- function() x
     set_inv <- function(val) inv <<- val
     get_inv <- function() inv
+    
+    # Return a list with inv. function handlers
     list(set = set, get = get, set_inv = set_inv, get_inv = get_inv)
 }
 
 
-## This function computes the inverse of a wrapped "matrix" returned by 
-# makeCacheMatrix above. It works this way: 
-# If the inverse has already been calculated (and the matrix has not changed), 
-# then this matrix is fetched from the cache, else it was calculated, cached and
-# returned as result
+## This function computes the inverse of a wrapped "matrix" returned 
+# by makeCacheMatrix above. It works this way: 
+# If the inverse has already been calculated (and its matrix has not 
+# changed), then this matrix is fetched from the cache, else it was 
+# calculated, cached and returned as result
 #
 #      Param x: a wrapped matrix object returned from makeCacheMatrix
 #      result : the inverse of the matrix wrapped inside x (x$get())
